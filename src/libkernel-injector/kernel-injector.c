da20ea5efadda0682a64da5e2413d4560a142da1
Vinamra Bhatia <vinamrabhatia8@gmail.com>
2019-06-26 19:18:44 +0530

    kernel-injector.h: Trapping in kernel mode with Syscalls

---
+++ src/libkernel-injector/kernel-injector.c
@@ -0,0 +1,2 @@
+//First, we make the traps to happen via Syscalls Plugin.
+//Later we checkout the injector
---
+++ src/libkernel-injector/kernel-injector.h
@@ -0,0 +1,29 @@
+#ifndef KERNEL_LIBINJECTOR_H
+#define KERNEL_LIBINJECTOR_H
+
+#include <glib.h>
+#include <config.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <sys/time.h>
+#include <libdrakvuf/libdrakvuf.h>
+
+#include "plugins/plugins.h"
+#include "private.h"
+
+class syscalls
+{
+
+private:
+    GSList* traps;
+
+public:
+    uint8_t reg_size;
+    output_format_t format;
+    os_t os;
+
+    syscalls(drakvuf_t drakvuf, const syscalls_config* config, output_format_t output);
+    ~syscalls();
+};
+
+#endif //KERNEL_LIBINJECTOR_H
